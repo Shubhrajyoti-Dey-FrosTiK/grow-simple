4669bb009776c3dd81df448b8a8448963e832f07
Eshaan Aggarwal <eshaanagg@gmail.com>
2023-01-12 21:19:06 +0530

    Bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic";
 import { Button } from "../components/components";
-import WebcamCapture from "../components/WebcamCapture.tsx";
+import WebcamCapture from "../components/WebcamCapture";
 
 export default function Home() {
   return (
