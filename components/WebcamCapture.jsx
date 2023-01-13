79bd29e0bf418b6558a8d58d42a6681be622965f
Eshaan Aggarwal <eshaanagg@gmail.com>
2023-01-13 11:29:35 +0530

    Move to only environment camera

--- components/WebcamCapture.jsx
+++ components/WebcamCapture.jsx
@@ -2,42 +2,42 @@ import React from "react";
 import Webcam from "react-webcam";
 
 const WebcamCapture = () => {
-  const [deviceId, setDeviceId] = React.useState({});
-  const [devices, setDevices] = React.useState([]);
+  // const [deviceId, setDeviceId] = React.useState({});
+  // const [devices, setDevices] = React.useState([]);
 
-  const handleDevices = React.useCallback(
-    (mediaDevices) =>
-      setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
-    [setDevices]
-  );
+  // const handleDevices = React.useCallback(
+  //   (mediaDevices) =>
+  //     setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
+  //   [setDevices]
+  // );
 
-  React.useEffect(() => {
-    navigator.mediaDevices.enumerateDevices().then(handleDevices);
-  }, [handleDevices]);
+  // React.useEffect(() => {
+  //   navigator.mediaDevices.enumerateDevices().then(handleDevices);
+  // }, [handleDevices]);
+
+  // return (
+  //   <>
+  //     {devices.map((device, key) => (
+  //       <div key={device.label}>
+  //         <Webcam
+  //           audio={false}
+  //           videoConstraints={{ deviceId: device.deviceId }}
+  //         />
+  //         {device.label || `Device ${key + 1}`}
+  //       </div>
+  //     ))}
+  //   </>
+  // );
+  const videoConstraints = {
+    facingMode: { exact: "environment" },
+  };
 
   return (
-    <>
-      {devices.map((device, key) => (
-        <div key={device.label}>
-          <Webcam
-            audio={false}
-            videoConstraints={{ deviceId: device.deviceId }}
-          />
-          {device.label || `Device ${key + 1}`}
-        </div>
-      ))}
-    </>
+    <div>
+      Video Camera
+      <Webcam videoConstraints={videoConstraints} />
+    </div>
   );
 };
 
 export default WebcamCapture;
-
-/*
-How to render to only allow environment camera
-
-const videoConstraints = {
-  facingMode: { exact: "environment" },
-};
-
-return <Webcam videoConstraints={videoConstraints} />;
-*/
