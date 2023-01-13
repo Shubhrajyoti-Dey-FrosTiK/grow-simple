3447f52c36379c81993f4e854ba5972944df5104
Eshaan Aggarwal <eshaanagg@gmail.com>
2023-01-13 11:42:05 +0530

    Option to Switch camera

--- components/WebcamCapture.jsx
+++ components/WebcamCapture.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Webcam from "react-webcam";
 
+const FACING_MODE_USER = { exact: "user" };
+const FACING_MODE_ENVIRONMENT = { exact: "environment" };
+
+const videoConstraints = {
+  facingMode: FACING_MODE_USER,
+};
+
 const WebcamCapture = () => {
   // const [deviceId, setDeviceId] = React.useState({});
   // const [devices, setDevices] = React.useState([]);
@@ -28,15 +35,40 @@ const WebcamCapture = () => {
   //     ))}
   //   </>
   // );
-  const videoConstraints = {
-    facingMode: { exact: "environment" },
-  };
+
+  // const videoConstraints = {
+  //   facingMode: { exact: "environment" },
+  // };
+
+  // return (
+  //   <div>
+  //     Video Camera
+  //     <Webcam videoConstraints={videoConstraints} />
+  //   </div>
+  // );
+
+  const [facingMode, setFacingMode] = React.useState(FACING_MODE_USER);
+
+  const handleClick = React.useCallback(() => {
+    setFacingMode((prevState) =>
+      prevState === FACING_MODE_USER
+        ? FACING_MODE_ENVIRONMENT
+        : FACING_MODE_USER
+    );
+  }, []);
 
   return (
-    <div>
-      Video Camera
-      <Webcam videoConstraints={videoConstraints} />
-    </div>
+    <>
+      <button onClick={handleClick}>Switch camera</button>
+      <Webcam
+        audio={false}
+        screenshotFormat="image/jpeg"
+        videoConstraints={{
+          ...videoConstraints,
+          facingMode,
+        }}
+      />
+    </>
   );
 };
 
